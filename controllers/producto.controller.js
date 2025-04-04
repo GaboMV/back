240b4ae526c359ec44dda4221c5cc418fb3e3b71
unknown <gabo171999@gmail.com>
2025-04-03 20:49:30 -1200

    Segundo commit del proyecto

--- controllers/producto.controller.js
+++ controllers/producto.controller.js
@@ -1,9 +1,9 @@
-const { Cliente } = require('../models');
+const { Producto } = require('../models');
 exports.obtenerproductos = async (req, res) => {
     const productos = await Producto.findAll();
     res.json(productos);
 };
 exports.crearproductos = async (req, res) => {
-    const producto = await producto.create(req.body);
+    const producto = await Producto.create(req.body);
     res.status(201).json(producto);
 };
